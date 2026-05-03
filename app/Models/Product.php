9460ebae1b3a154a3b0ea5aaf3f39369c2efb4a7
santostorresfabricio-max <santostorresfabricio@gmail.com>
2026-05-03 13:41:28 -0500

    Se creo el repo para el Proyecto tienda-api

---
+++ app/Models/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function orders(){
+        return $this->belongsToMany(Order::class)->withPivot('quantity', 'price')->withTimestamps();
+    }
+}
